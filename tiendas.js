191068fa4caa86b53a05e13e2be6d976de1b7f0a
ScavaMS <marinaschiapparr@gmail.com>
2023-06-28 16:21:54 -0300

    Página 2.0.0

--- tiendas.js
+++ tiendas.js
@@ -22,4 +22,7 @@ document.getElementById("siguiente").addEventListener("click", function asdfg() 
 
 
 
+document.getElementById("boton_creditos").addEventListener("click", function() {
+    document.querySelector(".container_creditos").classList.toggle("creditos-display");
+});
 
